69ab880725b994095d6e342f2f5db303acb196b9
Kalomiris <kalomirisgiannis@gmail.com>
2018-01-22 20:03:11 +0200

    Added compare method. The method gets two integers and return 0 or 1 depending on which combination of the inputs gives the largest number.

--- src/com/kalomiris/LargestCombination.java
+++ src/com/kalomiris/LargestCombination.java
@@ -1,4 +1,20 @@
 package com.kalomiris;
 
 public class LargestCombination {
+
+    public int compare(int a,int b){
+        String strA = Integer.toString(a);
+        String strB = Integer.toString(b);
+
+        String sumA = strA + strB;
+        int sumAA = Integer.parseInt(sumA);
+        String sumB = strB + strA;
+        int sumBB = Integer.parseInt(sumB);
+
+        if(sumAA >= sumBB){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
